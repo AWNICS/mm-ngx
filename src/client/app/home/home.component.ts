34e6ae14ea15f6ddad0db7e46ada14acaa1fe279
Arun Gadag <arun.gadag@awnics.com>
2017-02-13 18:48:04 +0530

    need to work on the modal window form

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, ViewChild, Output } from '@angular/core';
+import { Component, ViewChild, Output, OnInit } from '@angular/core';
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 
 import { OrderWindowComponent } from '../order-window/order-window.component';
+import { LocationService } from '../shared/location/location.service';
+import { Location } from '../shared/location/location';
 /**
  * This class represents the lazy loaded HomeComponent.
  */
@@ -11,9 +13,14 @@ import { OrderWindowComponent } from '../order-window/order-window.component';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css']
 })
-export class HomeComponent{ 
+export class HomeComponent implements OnInit { 
 
-  items:Array<string>;
+  constructor(private locationService: LocationService) {}
+
+  locations:Location[];
+  loc: string;
+
+  //items:Array<string>;
   name:string = 'Mesomeds';
   @Output() tel:number;
 
@@ -29,7 +36,21 @@ export class HomeComponent{
         if( result == true || tel.toString().length < 10 || tel.toString().match(/^\s*$/g)) {
           alert;
         } else {
+          console.log("Number you have entered is " + tel);
           this.modalHtml.open();
         }
+    }
+
+    setLocation(value:string) {
+      var setLocation = value;
+      console.log("This is new value:" + " " + setLocation);
+    }
+    
+    getLocations(): void {
+        this.locationService.getLocation().subscribe(locations => this.locations = locations);
+    }
+
+    ngOnInit():void {
+        this.getLocations();
     }
 }
--- src/client/app/order-window/order-window.component.ts
+++ src/client/app/order-window/order-window.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewChild, Input } from '@angular/core';
+import { Component, ViewChild, Input, OnInit, Output } from '@angular/core';
+import { OrderRequest } from './order-request';
+import { OrderRequestService } from './order-request.service';
 
 /**
  * This class represents the lazy loaded ModalComponent.
@@ -9,13 +11,41 @@ import { Component, ViewChild, Input } from '@angular/core';
   templateUrl: 'order-window.component.html',
   styleUrls: ['order-window.component.css'],
 })
-export class OrderWindowComponent {
+export class OrderWindowComponent implements OnInit{
+
+    selectDetails: OrderRequest;
+
+    constructor(private orderRequestService: OrderRequestService) {}
+
+    ngOnInit() {
+    }
 
     @Input() result:number;
 
     @ViewChild('modal')
     modal: OrderWindowComponent;
     open(){
+        console.log("This is coming from order window component: " + this.result);
         this.modal.open();
+    }
+
+    onSubmit(fullName:string, watel:number, mail:string, uFile:any, manual:string, check:boolean) {
+        console.log("Phone number from home component: " + this.result 
+        + "\nFullname is: " + fullName 
+        + "\n Whatsapp number is: " + watel 
+        + "\n Mail ID: " + mail
+        + "\n File uploaded: " + uFile
+        + "\n Manual instructions: " + manual
+        + "\n Checkbox: " + check);
+        this.modal.close();
+    }
+
+    requestCallback(tel:number) {
+        console.log("This number comes from requestCallback function" +tel);
+        this.modal.close();
+    }
+
+    close() {
+        this.modal.close();
     }
  }
