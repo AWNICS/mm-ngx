6290c369edc4d70fc22bf1631d382286d52bab31
niluroy <nilu.kumari@awnics.com>
2018-09-27 11:30:38 +0530

    payment component content added, audits created for appear call, modal window added for case notes

--- src/client/app/payment/payment.component.ts
+++ src/client/app/payment/payment.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { SecurityService } from '../shared/services/security.service';
+import { ChatService } from '../chat/chat.service';
+import { SharedService } from '../shared/services/shared.service';
 
 @Component({
     moduleId: module.id,
@@ -11,8 +16,28 @@ import { NavbarComponent } from '../shared/navbar/navbar.component';
 export class PaymentComponent implements OnInit {
 
     @ViewChild(NavbarComponent) navbarComponent: NavbarComponent;
+    visitorId: number;
+    bills: any[] = [];
+
+    constructor(private route: ActivatedRoute, private router: Router,
+        private securityService: SecurityService, private chatService: ChatService,
+        private sharedService: SharedService) { }
 
     ngOnInit() {
         this.navbarComponent.navbarColor(0, '#6960FF');
+        this.visitorId = +this.route.snapshot.paramMap.get('id');// this will give the visitor id
+        const cookie = this.securityService.getCookie('userDetails');
+        if (cookie === '') {
+            this.router.navigate([`/login`]);
+        } else {
+            this.getBills();
+        }
+    }
+
+    getBills() {
+        this.sharedService.getBills(this.visitorId)
+            .subscribe((billings) => {
+                this.bills = billings;
+            });
     }
 }
